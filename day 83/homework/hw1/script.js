e196f33a2e5c095d478db21d9c6a87737b1db159
nika123n <nikoloznikoloz234@gmail.com>
2025-01-02 14:02:02 +0400

    commit

---
+++ day 83/homework/hw1/script.js
@@ -0,0 +1,10 @@
+//მომხმარებელი შეჰყავს ერთი რიცხვი და უნდა გამოიტანოს ინფორმაცია, არის ეს რიცხვი ლუწი თუ კენტი.
+
+let number = prompt('Enter number: ')
+
+if(number % 2 == 0){
+    console.log('Even')
+}
+else{
+    console.log('Odd')
+}
