dea7f19f1f184a978cc82ea7c59fda1d623a8990
nika123n <nikoloznikoloz234@gmail.com>
2025-01-26 18:48:59 +0400

    commit

---
+++ day 094/homework/script.js
@@ -0,0 +1,8 @@
+//დავალება: შექმენით ფუნქცია, რომელიც მომხმარებელს მოთხოვს ველექტრონული ფოსტის წარდგენა, შემოტანის შემდეგ დაუწერეთ ტექსტი - 'access granted'.
+
+func = () => {
+    let email = prompt('Enter your email: ')
+    console.log('access granted')
+}
+
+func()
---
+++ day 094/homework/script2.js
@@ -0,0 +1,11 @@
+//დავალება: დაწერეთ პროგრამა, რომელიც იღებს ორი რიცხვს და ამოწმებს, თითეული რიცხვი უდრის თუ არა ერთმანეთს და ასევე ამოწმებს დიდია ან პატარა.
+
+func = () => {
+    let number = prompt('Enter number: ')
+    let number2 = prompt('Enter number: ')
+
+    console.log(number = number2)
+    console.log(number > number2)
+    console.log(number < number2)
+    
+}
